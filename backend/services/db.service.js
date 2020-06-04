b2ff553a78a53762b3d67b8e8420c936337b2bcd
Or Dotan <ordel23@gmail.com>
2020-06-04 17:31:35 +0300

    with git ignore

--- backend/services/db.service.js
+++ backend/services/db.service.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const config  =  require('../config')
+const config = require('../config')
 
 module.exports = {
     getCollection
@@ -19,11 +19,11 @@ async function getCollection(collectionName) {
 async function connect() {
     if (dbConn) return dbConn;
     try {
-        const client = await MongoClient.connect(config.dbURL, {useNewUrlParser: true});
+        const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology });
         const db = client.db(dbName);
         dbConn = db;
         return db;
-    } catch(err) {
+    } catch (err) {
         console.log('Cannot Connect to DB', err)
         throw err;
     }
